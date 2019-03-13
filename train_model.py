cf5737d2fbd85615334f50b6ae259c4d37c465c0
Joe Love <git@joelove.uk>
2019-03-13 19:20:15 +0000

    split up training script

--- train_model.py
+++ train_model.py
@@ -8,10 +8,32 @@ from PIL import Image
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
-def train_model(src_directory='training_images'):
-    start_time = time.time()
-    print("Creating training data")
+def save_model(model):
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    model.save_weights("model.h5")
+    print("Saved model to disk")
 
+
+def create_model(x_train, y_train):
+    print("Training model")
+    model = Sequential()
+    model.add(Conv2D(48, (5, 5), padding="same", input_shape=(48, 48, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(50, (5, 5), padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(500, activation="relu"))
+    model.add(Dense(26, activation="softmax"))
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    model.fit(np.array(x_train), np.array(y_train), validation_split=0.1, epochs=10, verbose=1)
+    save_model(model)
+
+
+def train_model(src_directory='training_images'):
+    print("Creating training data")
+    start_time = time.time()
     x_train = []
     y_train = []
     image_directories = glob.glob(src_directory + "/*")
@@ -19,38 +41,19 @@ def train_model(src_directory='training_images'):
     for _, directory in enumerate(image_directories):
         letter = directory.split('/')[-1]
         image_files = glob.glob(directory + "/*.png")
+        letter_images = []
 
         for _, filename in enumerate(image_files):
             try:
                 image = cv2.imread(filename)
-                x_train.append(image)
-                y_train.append(letter)
+                letter_images.append(image)
             except:
                 pass
 
-    print("Training model")
+        x_train.append(np.array(letter_images))
+        y_train.append(letter)
 
-    model = Sequential()
-
-    model.add(Conv2D(20, (5, 5), padding="same", input_shape=(20, 20, 1), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(50, (5, 5), padding="same", activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(500, activation="relu"))
-    model.add(Dense(26, activation="softmax"))
-
-    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-
-    model.fit(x_train, y_train, validation_split=0.1, epochs=10, verbose=1)
-
-    model_json = model.to_json()
-    with open("model.json", "w") as json_file:
-        json_file.write(model_json)
-
-    model.save_weights("model.h5")
-
-    print("Saved model to disk")
+    create_model(x_train, y_train)
 
 
 if __name__ == '__main__':
